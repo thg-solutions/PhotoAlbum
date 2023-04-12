62109cb49275c367d7e40165ca2203941d16fb7c
Thomas Grund <thomas.grund@thg-solutions.de>
2023-04-12 11:52:25 +0200

    added Spring @Sql annotation to populate DB for tests.

--- src/test/java/de/thg/photoalbum/repositories/ImageRepositoryTest.java
+++ src/test/java/de/thg/photoalbum/repositories/ImageRepositoryTest.java
@@ -4,12 +4,17 @@ import de.thg.photoalbum.model.Image;
 import de.thg.photoalbum.testcontainers.AbstractContainerBaseTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Sql(statements = {"insert into image (creation_date, filename, version) values (\'Date_1\', \'Image_1\', 0)",
+        "insert into image (creation_date, filename, version) values (\'Date_2\', \'Image_2\', 0)"})
 @SpringBootTest
 class ImageRepositoryTest extends AbstractContainerBaseTest {
 
@@ -18,14 +23,29 @@ class ImageRepositoryTest extends AbstractContainerBaseTest {
 
     @Test
     @Transactional
+    void testFindImage() {
+        Optional<Image> image1 = imageRepository.findByFilename("Image_2");
+        assertThat(image1).isPresent();
+        assertThat(image1.get().getCreationDate()).isEqualTo("Date_2");
+    }
+
+    @Test
+    @Transactional
+    void testFindBaNameStartingWith() {
+        List<Image> images = imageRepository.findByFilenameStartingWith("Image");
+        assertThat(images).hasSize(2);
+    }
+
+    @Test
+    @Transactional
     void saveImage() {
-        assertThat(imageRepository.findAll()).isEmpty();
+        assertThat(imageRepository.findAll()).hasSize(2);
         Image image = new Image();
-        image.setCreationDate("1");
+        image.setCreationDate("3");
         Image savedImage = imageRepository.saveAndFlush(image);
         assertThat(savedImage).isNotNull();
-        assertThat(savedImage.getCreationDate()).isEqualTo("1");
-        assertThat(imageRepository.findAll()).hasSize(1);
+        assertThat(savedImage.getCreationDate()).isEqualTo("3");
+        assertThat(imageRepository.findAll()).hasSize(3);
         assertThat(imageRepository.findAll()).contains(savedImage);
         assertThat(savedImage).isEqualTo(image);
         assertThat(savedImage).isNotSameAs(image);
