3b4fbb6fe6f45886dec709568813452bfb03b945
Thomas Grund <thomas.grund@thg-solutions.de>
2023-09-28 14:26:26 +0200

    minor corrections

--- src/test/java/de/thg/photoalbum/integration/DockerComposeContainerIT.java
+++ src/test/java/de/thg/photoalbum/integration/DockerComposeContainerIT.java
@@ -3,11 +3,14 @@ package de.thg.photoalbum.integration;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.testcontainers.containers.ComposeContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.File;
+
+import static io.restassured.RestAssured.given;
 
 @Testcontainers
 public class DockerComposeContainerIT {
@@ -15,13 +18,14 @@ public class DockerComposeContainerIT {
     @Container
     public static ComposeContainer composeContainer =
             new ComposeContainer(new File("src/main/resources/compose.yaml"))
-                    .withExposedService("photoalbum-1", 8080);
+                    .withExposedService("photoalbum-1", 8080)
+                    .withLocalCompose(true);
 
     private String host;
     private int port;
 
     @BeforeEach
-    void Setup() {
+    void setup() {
         host = composeContainer.getServiceHost("photoalbum-1", 8080);
         port = composeContainer.getServicePort("photoalbum-1", 8080);
     }
@@ -30,8 +34,8 @@ public class DockerComposeContainerIT {
     void dockerNetworkStarted() {
         RestAssured.baseURI = host;
         RestAssured.port = port;
-//        given()
-//                .when().get("http://localhost:" + port + "/photoalbum/photos")
-//                .then().assertThat().statusCode(HttpStatus.OK.value());
+        given()
+                .when().get("http://localhost:" + port + "/photoalbum/photos")
+                .then().assertThat().statusCode(HttpStatus.OK.value());
     }
 }
